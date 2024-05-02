9aef94a5dd71d49886ef8e758a07bf81c8685cda
Youssof112 <https://github.com/Youssof112>
2024-05-02 15:44:46 +0300

    Youssof configured Port D

--- Project/SRC/main.c
+++ Project/SRC/main.c
@@ -32,5 +32,12 @@ void PC_UAR2_init(){
 	
 	UART2_CTL_R 				=0X0070;			
 	UART2_LCRH_R 				=0x0301;
+/* in PORTA UART0 was on pins 0 AND 1 SO WE USED 0X03 AS a mask 
+	but here in PORTD UART2 is on pins 6 and 7 so we use 0xC0 as a mask */
+	
+	GPIO_PORTD_AFSEL_R  |=0xC0;
+	GPIO_PORTD_PCTL_R		= (GPIO_PORTA_PCTL_R &0X00FFFFFF )+ 0X11000000;
+	GPIO_PORTD_DEN_R |=0XC0;
+	GPIO_PORTD_AMSEL_R &= ~0XC0;
 	
 }
